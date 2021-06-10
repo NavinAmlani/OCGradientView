0e26b7c49baa56baa435e4c888f87075de08b3bc
Navin <navin@itoneclick.com>
2021-06-10 19:10:59 +0530

    custom file added

---
+++ OCGradientView/Classes/CustomGradientView.swift
@@ -0,0 +1,93 @@
+//
+//  CustomGradientView.swift
+
+import Foundation
+import UIKit
+
+@IBDesignable public class CustomGradientView: UIView {
+    
+    // MARK: - Gradient
+    
+    @IBInspectable public var firstColor: UIColor = UIColor.white {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable public var secondColor: UIColor = UIColor.white {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable public var horizontalGradient: Bool = false {
+        didSet {
+            updateView()
+        }
+    }
+    
+    public override class var layerClass: AnyClass {
+        get {
+            return CAGradientLayer.self
+        }
+    }
+    
+    public func updateView() {
+        let layer = self.layer as! CAGradientLayer
+        layer.colors = [ firstColor.cgColor, secondColor.cgColor ]
+        
+        if (horizontalGradient) {
+            layer.startPoint = CGPoint(x: 0.0, y: 0.5)
+            layer.endPoint = CGPoint(x: 1.0, y: 0.5)
+        } else {
+            layer.startPoint = CGPoint(x: 0, y: 0)
+            layer.endPoint = CGPoint(x: 0, y: 1)
+        }
+    }
+    
+    // MARK: - Border
+    
+    @IBInspectable public var borderColor: UIColor = UIColor.clear {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable public var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable public var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    // MARK: - Shadow
+    
+    @IBInspectable public var shadowOpacity: CGFloat = 0 {
+        didSet {
+            layer.shadowOpacity = Float(shadowOpacity)
+        }
+    }
+    
+    @IBInspectable public var shadowColor: UIColor = UIColor.clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+    
+    @IBInspectable public var shadowRadius: CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+    
+    @IBInspectable public var shadowOffsetY: CGFloat = 0 {
+        didSet {
+            layer.shadowOffset.height = shadowOffsetY
+        }
+    }
+}
